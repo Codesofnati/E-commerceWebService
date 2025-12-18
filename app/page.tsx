7fa95831565158837fd47cf1bb699d2e6a4bcc65
Nathnael asefa <Nathnaelasefa@gmail.com>
2025-12-18 19:43:17 +0300

    Firt eddition

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Login from "./auth/Login";
+import Registration from "./auth/Registration";
+
+export default function Home() {
+  return (
+    <div>
+      <Registration/>
+    </div>
+  );
+}
